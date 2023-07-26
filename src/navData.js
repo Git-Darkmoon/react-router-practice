91ac8c7f22b3087550484a96d08f63b66f180dd8
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-25 23:12:07 -0500

    Created this archive for making the navbar dynamic

---
+++ src/navData.js
@@ -0,0 +1,24 @@
+const navbarData = [
+  {
+    id: self.crypto.randomUUID(),
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    name: "Contact",
+    path: "/contact",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    name: "Our Team",
+    path: "/team",
+  },
+]
+
+export default navbarData
