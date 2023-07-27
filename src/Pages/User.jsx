6904cd2065224935b3678533219b28c02e6ddac0
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-27 13:20:51 -0500

    styles: user card finished.

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -13,34 +13,44 @@ function User() {
   const { name, username, email, address, phone, company } = actualUser
 
   return (
-    <article className="infoCard">
-      <header className="infoCard__header">
-        <img src={profilePic} alt={"undraw svg showing an astronaut"} />
-        <div className="basicInfo">
-          {name}
-          <h2>{name}</h2>
-          <h4>USA, {address.city}</h4>
+    <section className="infoCard__container">
+      <article className="infoCard">
+        <header className="infoCard__header">
+          <picture className="profilePic__container">
+            <img
+              src={profilePic}
+              alt={"undraw svg showing an astronaut"}
+              className="profilePic"
+            />
+          </picture>
+          <div className="basicInfo">
+            <h2 className="basicInfo__name">{name}</h2>
+            <h4 className="basicInfo__city">USA, {address.city}</h4>
+          </div>
+        </header>
+        <div className="lineContainer">
+          <div className="lineDivision"></div>
         </div>
-      </header>
-      <section>
-        <div className="row">
-          <h3 className="label">Username:</h3>
-          <h3>{username}</h3>
-        </div>
-        <div className="row">
-          <h3 className="label">Email:</h3>
-          <h3>{email}</h3>
-        </div>
-        <div className="row">
-          <h3 className="label">Phone:</h3>
-          <h3>{phone}</h3>
-        </div>
-        <div className="row">
-          <h3 className="label">Company:</h3>
-          <h3>{company.name}</h3>
-        </div>
-      </section>
-    </article>
+        <section>
+          <div className="row">
+            <h3 className="label">Username:</h3>
+            <h3 className="row__value">{username}</h3>
+          </div>
+          <div className="row">
+            <h3 className="label">Email:</h3>
+            <h3 className="row__value">{email}</h3>
+          </div>
+          <div className="row">
+            <h3 className="label">Phone:</h3>
+            <h3 className="row__value">{phone}</h3>
+          </div>
+          <div className="row">
+            <h3 className="label">Company:</h3>
+            <h3 className="row__value">{company.name}</h3>
+          </div>
+        </section>
+      </article>
+    </section>
   )
 }
 export default User
