1b6622b2512161aa4b344eb1fc83462651f90fba
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-26 23:51:43 -0500

    Wrapped the entire app with the AppProvider comp.

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import App from "./App.jsx"
 import "./index.scss"
 import { BrowserRouter } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import AppProvider from "./context.jsx"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <AppProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </AppProvider>
     </QueryClientProvider>
   </BrowserRouter>
 )
